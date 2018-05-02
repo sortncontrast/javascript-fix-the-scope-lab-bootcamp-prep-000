565c74b0e5bee1e3665a5430fe6a9502fd845fef
tonyamagar@gmail.com <tonyamagar@gmail.com>
2018-05-02 01:19:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
+<<<<<<< HEAD
 function myAnimal() {
   var animal = 'dog';
   return animal;
 }
 
+=======
+>>>>>>> 095371fc089159d09f132ebd1dbb756805c97600
 function yourAnimal() {
   var animal = 'cat';
   return animal;
 }
 
+var animal = 'dog'
+
+function myAnimal() {
+  return animal
+}
+
 function add2(n) {
   const two = 2;
   return n + two;
+<<<<<<< HEAD
+=======
+
+  // Feel free to move things around!
+>>>>>>> 095371fc089159d09f132ebd1dbb756805c97600
 }
 var funkyFunction = function() {
     return function() {
